d139c824e8a929a5ea834c1fa0a91a64d3b6e316
Abbas Roholamin <abbasroholamin76@gmail.com>
2022-10-15 09:22:11 +0430

    overwrite base font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
+        fontSize: {
+            sm: ["0.75rem", "1rem"],
+            base: ["0.875rem", "1.25rem"],
+            lg: ["1rem", "1.5rem"],
+            xl: ["1.125rem", "1.75rem"],
+            "2xl": ["1.25rem", "1.75rem"],
+            "3xl": ["1.5rem", "2rem"],
+            "4xl": ["1.875rem", "2.25rem"],
+            "5xl": ["2.25rem", "2.5rem"],
+        },
         extend: {},
     },
     plugins: [require("tailwindcss-flip"), require("@tailwindcss/forms")],
